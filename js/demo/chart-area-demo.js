7e9e519362a51d937ab86c143f96f19a34b97e06
Facundo <facundocampodonico@mail.com>
2020-12-03 23:27:08 -0300

    listo grafico

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -28,6 +28,16 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 }
 
 // Area Chart Example
+
+var data=localStorage.getItem('graficoPedidos');
+var dat=data.replace("[","");
+var dataenvio=dat.replace("]","");
+var separador=",";
+   var dataenvio = dataenvio.split(separador);
+      for (var i=0; i < dataenvio.length; i++) {
+      document.write(dataenvio[i] + ",");
+   }
+
 var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
@@ -46,8 +56,10 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      //data: [0, 10, 15, 20, 30, 8, 7, 3, 4, 5, 0, 10],//aca necesito un array del back
-      data:localStorage.getItem('graficoPedidos'),
+       //data: [99,0,4,4,4,4,4,4,4,4,4,2]
+             // [0,0,4,4,4,4,4,4,4,4,4,2]
+      data:dataenvio,
+
     }],
   },
   options: {
@@ -70,11 +82,13 @@ var myLineChart = new Chart(ctx, {
           drawBorder: false
         },
         ticks: {
-          maxTicksLimit: 7
+          maxTicksLimit: 12
         }
       }],
       yAxes: [{
         ticks: {
+          min: 0,
+          max: 10,
           maxTicksLimit: 5,
           padding: 10,
           // Include a dollar sign in the ticks
@@ -111,7 +125,7 @@ var myLineChart = new Chart(ctx, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ':' + number_format(tooltipItem.yLabel);
         }
       }
     }
